6a1e06dd4e85ba56ffd57b554dd8c9b941911c20
ume24 <yoomee2009@naver.com>
2025-11-17 22:09:10 +0900

    fix: 패키지 정리, .gradle 삭제

---
+++ src/main/java/com/matchTime/schedule/Time.java
@@ -0,0 +1,15 @@
+package com.matchTime.schedule;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Time {
+
+    private final String name;
+
+}
+
+
