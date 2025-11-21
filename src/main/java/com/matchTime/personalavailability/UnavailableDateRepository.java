ec05f44614795f30c61d56e1258c6a1f4b533c67
유미 <134575309+ume24@users.noreply.github.com>
2025-11-21 23:28:08 +0900

    Delete src/main/java/com/matchTime directory

--- src/main/java/com/matchTime/personalavailability/UnavailableDateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.matchtime.personalavailability;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface UnavailableDateRepository extends JpaRepository<UnavailableDate, Long> {
-    List<UnavailableDate> findByUserId(Long userId);
-}
