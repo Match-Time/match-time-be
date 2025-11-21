267d669d69179d99b5cc46385df8b1b0c90f0d18
유미 <134575309+ume24@users.noreply.github.com>
2025-11-21 23:27:56 +0900

    Create MatchtimeApplication.java

---
+++ src/main/java/com/matchtime/MatchtimeApplication.java
@@ -0,0 +1,13 @@
+package com.matchtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchtimeApplication.class, args);
+	}
+
+}
